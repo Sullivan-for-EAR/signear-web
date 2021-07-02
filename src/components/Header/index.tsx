d3f7184ff478034158bc581df9383afa81511b0b
vewevteen <vewevteen0414@gmail.com>
2021-07-02 16:22:47 +0900

    Wrapper 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Box from '@/components/Box';
 import Flex from '@/components/Flex';
 
+import { ReactComponent as AlarmIcon } from '../../images/icons-alarm.svg';
+
 const DefaultHeader: React.FC = () => {
   return (
     <Box mb="38px">
@@ -9,7 +11,14 @@ const DefaultHeader: React.FC = () => {
         <Box fontSize="24px" fontWeight="700">
           헤더 타이틀
         </Box>
-        <Box>데이터</Box>
+        <Flex>
+          <Box mr="33px" width="14px" height="16px">
+            <AlarmIcon />
+          </Box>
+          <Box paddingLeft="50px" borderLeft="2px solid #DFE0EB">
+            강동 1 테스트
+          </Box>
+        </Flex>
       </Flex>
       <Box color="#9FA2B4">OOO님 에게 오늘 예약된 통역 목록입니다.</Box>
     </Box>
--- src/components/Wrapper/index.tsx
+++ src/components/Wrapper/index.tsx
@@ -1,28 +1,13 @@
 import Box, { BoxProps } from '@/components/Box';
 
 import styled from 'styled-components';
-import { variant } from 'styled-system';
 
 const Wrapper = styled(Box).attrs<BoxProps>({
   display: 'flex',
-})<BoxProps>(
-  variant({
-    variants: {
-      default: {
-        px: '32px',
-      },
-      desktopOnly: {
-        px: [null, '32px'],
-      },
-    },
-  })
-);
-
-Wrapper.defaultProps = {
+  px: '32px',
   flexDirection: 'row',
   flexWrap: 'wrap',
-  variant: 'default',
   maxWidth: 'maxWidth',
-};
+})<BoxProps>``;
 
 export default Wrapper;
