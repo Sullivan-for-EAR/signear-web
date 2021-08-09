3cbb5622b1efbe6273a3277d8846a920a92af252
vewevteen <vewevteen0414@gmail.com>
2021-08-09 21:21:22 +0900

    사이드바 수정

--- src/components/DefaultSidebar/index.tsx
+++ src/components/DefaultSidebar/index.tsx
@@ -3,22 +3,35 @@ import Flex from '@/components/Flex';
 
 import Tab from '@/components/Tab';
 
+import { ReactComponent as Logo } from '../../images/icons-logo.svg';
+
 const DefaultSidebar: React.FC = () => {
   return (
-    <Flex backgroundColor="#F9F9F9" py="48px" minWidth="225px">
+    <Flex
+      flexDirection="column"
+      backgroundColor="#F9F9F9"
+      p="48px 45px"
+      minWidth="150px"
+    >
+      <Logo />
       <Tab
+        mt="80px"
         links={[
           {
             text: '오늘의 이어',
-            href: `/`,
+            href: '/',
           },
           {
             text: '예약관리',
-            href: `/reservation`,
+            href: '/reservation',
           },
           {
             text: '사용자 관리',
-            href: `/userManage`,
+            href: '/userManage',
+          },
+          {
+            text: '통역사 관리',
+            href: '/interpreterManage',
           },
         ]}
       />
--- src/components/Tab/index.tsx
+++ src/components/Tab/index.tsx
@@ -1,37 +1,72 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { BoxProps } from '@/components/Box';
+import { Link, useLocation } from 'react-router-dom';
+import Box, { BoxProps } from '@/components/Box';
 import Flex from '@/components/Flex';
 import ActiveLink from '@/components/ActiveLink';
+
+import { ReactComponent as CalendarIcon } from '../../images/icons-calendar.svg';
+import { ReactComponent as ReservationIcon } from '../../images/icons-reservation.svg';
+import { ReactComponent as UserIcon } from '../../images/icons-user.svg';
+import { ReactComponent as SettingIcon } from '../../images/icons-setting.svg';
 
 interface LinkProps {
   href: string;
   text: string;
 }
 
+interface IconProps {
+  type: string;
+}
+
+const Icon: React.FC<IconProps> = ({ type }) => {
+  const IconType = type.replace('/', '');
+  switch (IconType) {
+    case 'reservation':
+      return <ReservationIcon />;
+    case 'userManage':
+      return <UserIcon />;
+    case 'interpreterManage':
+      return <SettingIcon />;
+    default:
+      return <CalendarIcon />;
+  }
+};
+
 const Tab: React.FC<
   { links: LinkProps[]; isSamePath?: boolean } & BoxProps
 > = ({ links, isSamePath, ...props }) => {
+  const location = useLocation();
   return (
     <Flex flexDirection="column" flex="1" {...props}>
-      {links.map(({ text, href }) => (
-        <Link key={text} to={href}>
-          <ActiveLink
+      {links.map(({ text, href }) => {
+        const isActiveLink = location.pathname === href;
+        return (
+          <Flex
             p="18px 0px"
-            mx="45px"
             borderBottom="1px solid #E5E5E5"
-            fontSize="20px"
-            fontWeight="500"
-            activeStyles={{
-              color: '#0A84FF',
-            }}
-            isSamePath={isSamePath}
-            href={href}
+            key={text}
+            alignItems="center"
           >
-            {text}
-          </ActiveLink>
-        </Link>
-      ))}
+            <Box color={isActiveLink ? '#0A84FF' : 'black'}>
+              <Icon type={href} />
+            </Box>
+            <Link to={href}>
+              <ActiveLink
+                ml="14px"
+                fontSize="20px"
+                fontWeight="500"
+                activeStyles={{
+                  color: '#0A84FF',
+                }}
+                isSamePath={isSamePath}
+                href={href}
+              >
+                {text}
+              </ActiveLink>
+            </Link>
+          </Flex>
+        );
+      })}
     </Flex>
   );
 };
